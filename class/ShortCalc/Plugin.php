2f8e85bfbc36b7ed9315eea5a82a3d01b0ecbf5a
Sander Baas <sander@implode.nl>
2017-07-10 15:47:38 +0200

    SNAPSHOT: calculator definition within wordpress demo
    This needs to be moved to WPPostCalculator class

--- class/ShortCalc/Plugin.php
+++ class/ShortCalc/Plugin.php
@@ -14,6 +14,94 @@ class Plugin {
 		add_action('init', array($this,'init'));
 		add_action('wp_ajax_get_calculator_result', array($this, 'getCalculatorResult'));
 		add_action('wp_ajax_nopriv_get_calculator_result', array($this, 'getCalculatorResult'));
+
+		add_action( 'cmb2_admin_init', array($this, 'registerCalculatorMetabox'));
+	}
+
+	public function registerCalculatorMetabox() {
+		$domain = $this->plugin_slug;
+		$prefix = $this->plugin_slug . '_';
+
+		$cmb = new_cmb2_box( array(
+			'id'            => $prefix . 'calculator_mb',
+			'title'         => esc_html__( 'Calculator Details', $domain ),
+			'object_types'  => array( 'shortcalc_calculator', ),
+		) );
+
+		$options = array();
+		foreach ($this->implementations['formulaParsers'] as $parser){
+			$options[$parser] = $parser;
+		}
+
+		$cmb->add_field(array(
+			'name' => 'Formula Parser',
+			'id'   => 'formula_parser',
+			'type' => 'select',
+			'options' => $options
+		) );
+
+		$cmb->add_field( array(
+			'name'       => esc_html__( 'Formula', $domain ),
+			'desc'       => esc_html__( 'field description (optional)', $domain ),
+			'id'         => $prefix . 'text',
+			'type'       => 'text',
+			'column' => true,
+		) );
+
+		$group_parameters = $cmb->add_field( array(
+			'id'          => $prefix . 'parameters',
+			'type'        => 'group',
+			// 'repeatable'  => false, // use false if you want non-repeatable group
+			'options'     => array(
+				'group_title'   => __( 'Parameter {#}', $domain ), // since version 1.1.4, {#} gets replaced by row number
+				'add_button'    => __( 'Add Another Parameter', $domain ),
+				'remove_button' => __( 'Remove Parameter', $domain ),
+				'sortable'      => true, // beta
+				// 'closed'     => true, // true to have the groups closed by default
+			),
+		) );
+
+		$cmb->add_group_field( $group_parameters, array(
+			'name' => 'Name',
+			'description' => __('Must match a parameter in the formula', $domain),
+			'id'   => 'name',
+			'type' => 'text',
+		) );
+
+		$cmb->add_group_field( $group_parameters, array(
+			'name' => 'Type of Element',
+			'id'   => 'element',
+			'type' => 'select',
+			'default' => 'input',
+			'options' => array(
+				'input' => __('Input', $domain),
+				'select' => __('Select', $domain),
+			),
+		) );
+
+		$cmb->add_group_field( $group_parameters, array(
+			'name' => 'Label',
+			'id'   => 'label',
+			'description' => __('The label is used on the calculator frontend', $domain),
+			'type' => 'text',
+			// 'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
+		) );
+
+		$cmb->add_group_field( $group_parameters, array(
+			'name' => 'Attributes',
+			'description' => __('HTML attributes, for example: value="Foo" required type="submit"', $domain),
+			'id'   => 'attribute',
+			'type' => 'textarea_small',
+			'repeatable' => false, // Repeatable fields are supported w/in repeatable groups (for most types)
+		) );
+
+		$cmb->add_group_field( $group_parameters, array(
+			'name' => 'Options',
+			'description' => __('Selectable options for select elements', $domain),
+			'id'   => 'options',
+			'type' => 'text',
+			'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
+		) );
 	}
 
 	public function registerImplementations($implementations) {
@@ -66,6 +154,45 @@ class Plugin {
 	}
 
 	private function registerPostTypes() {
+		$this->registerCalculatorPostType();
+	}
+
+	private function registerCalculatorPostType() {
+		$domain = $this->plugin_slug;
+		$labels = array(
+			'name'               => _x( 'Calculators', 'post type general name', $domain ),
+			'singular_name'      => _x( 'Calculator', 'post type singular name', $domain ),
+			'menu_name'          => _x( 'Calculators', 'admin menu', $domain ),
+			'name_admin_bar'     => _x( 'Calculator', 'add new on admin bar', $domain ),
+			'add_new'            => _x( 'Add New', 'calculator', $domain ),
+			'add_new_item'       => __( 'Add New Calculator', $domain ),
+			'new_item'           => __( 'New Calculator', $domain ),
+			'edit_item'          => __( 'Edit Calculator', $domain ),
+			'view_item'          => __( 'View Calculator', $domain ),
+			'all_items'          => __( 'All Calculators', $domain ),
+			'search_items'       => __( 'Search Calculators', $domain ),
+			'parent_item_colon'  => __( 'Parent Calculators:', $domain ),
+			'not_found'          => __( 'No calculators found.', $domain ),
+			'not_found_in_trash' => __( 'No calculators found in Trash.', $domain )
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'description'        => __( 'Description.', $domain ),
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'calculator' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => null,
+			'supports'           => array( 'title' )
+		);
+
+		register_post_type( 'shortcalc_calculator', $args );
 	}
 
 	private function registerTaxonomies() {
--- shortcalc.php
+++ shortcalc.php
@@ -17,6 +17,16 @@ namespace ShortCalc;
 require 'vendor/autoload.php';
 
 spl_autoload_register(function ($class_name) {
+	if (preg_match('/^CMB2_Type_/', $class_name) == 1) {
+		$filename = __DIR__ . '/vendor/webdevstudios/cmb2/includes/types/' . $class_name . '.php';
+		include($filename);
+		return;
+	}
+	if (preg_match('/^CMB2/', $class_name) == 1) {
+		$filename = __DIR__ . '/vendor/webdevstudios/cmb2/includes/' . $class_name . '.php';
+		include($filename);
+		return;
+	}
 	$filename = __DIR__ . '/class/' . str_replace('\\', '/', $class_name) . '.php';
 	$filename = apply_filters('shortcalc_autoload_register', $filename, $class_name);
 	include($filename);
@@ -24,6 +34,10 @@ spl_autoload_register(function ($class_name) {
 
 if ( ! defined( 'WPINC' ) ) {
 	die;
+}
+
+if ( file_exists( dirname( __FILE__ ) . '/vendor/webdevstudios/cmb2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/vendor/webdevstudios/cmb2/init.php';
 }
 
 register_activation_hook(__FILE__, array('ShortCalc\Plugin', 'install' ) );
