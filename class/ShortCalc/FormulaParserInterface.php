ffaad0a8d851aa8e759e3902bdd3a6e44f0c96c8
Sander Baas <sander@implode.nl>
2017-07-13 11:19:40 +0200

    add extractparameters from formula function
    also force formula to string for HoaMath

--- class/ShortCalc/FormulaParserInterface.php
+++ class/ShortCalc/FormulaParserInterface.php
@@ -5,5 +5,6 @@ interface FormulaParserInterface {
 	public function __construct();
 	public function setFormula($formula);
 	public function setParameter(String $key, $value);
+	public static function extractParameters($formula);
 	public function getResult();
 }
--- class/ShortCalc/FormulaParsers/HoaMath.php
+++ class/ShortCalc/FormulaParsers/HoaMath.php
@@ -4,20 +4,23 @@ use \ShortCalc\FormulaParserInterface;
 use \Rezzza\Formulate\Formula;
 
 class HoaMath implements FormulaParserInterface {
-	public $name;
 	private $formula;
 
-	public function __construct() {
-		
-	}
+	public function __construct() {}
 
 	public function setFormula($formula) {
 		$formula = new Formula($formula);
-		$this->formula = $formula;
+		$this->formula = (string)$formula;
 	}
 
 	public function setParameter(String $key, $value) {
 		$this->formula->setParameter($key, $value);
+	}
+
+	public static function extractParameters($formula) {
+		$formula = (string)$formula;
+		preg_match_all('/\{\{([^\}])*\}\}/', $formula, $matches);
+		return $matches[1];
 	}
 
 	public function getResult() {
