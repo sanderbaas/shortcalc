63398aedb7025292984100dbdc4ce1144a5b5688
Sander Baas <sander@implode.nl>
2017-07-20 13:33:07 +0200

    format cmb2 code

--- class/ShortCalc/Calculators/WPPostCalculator.php
+++ class/ShortCalc/Calculators/WPPostCalculator.php
@@ -22,11 +22,11 @@ class WPPostCalculator extends CalculatorCore implements CalculatorInterface {
 		$domain = $plugin->plugin_slug;
 		$prefix = $plugin->plugin_slug . '_';
 
-		$cmb = new_cmb2_box( array(
-			'id'            => $prefix . 'calculator_mb',
-			'title'         => esc_html__( 'Calculator Details', $domain ),
-			'object_types'  => array( 'shortcalc_calculator', ),
-		) );
+		$cmb = new_cmb2_box(array(
+			'id' => $prefix . 'calculator_mb',
+			'title' => __('Calculator Details', $domain),
+			'object_types' => array('shortcalc_calculator'),
+		));
 
 		$options = array();
 		foreach ($plugin->implementations['formulaParsers'] as $parser){
@@ -34,108 +34,105 @@ class WPPostCalculator extends CalculatorCore implements CalculatorInterface {
 			$options[$value] = $value;
 		}
 
-		$cmb->add_field( array(
-			'name'       => esc_html__( 'Formula', $domain ),
-			'desc'       => esc_html__( 'Formula parsable by Formula Parser above. Docs: https://hoa-project.net/En/Literature/Hack/Math.html', $domain ),
-			'id'         => $prefix . 'formula',
-			'type'       => 'text',
+		$cmb->add_field(array(
+			'name' => __('Formula', $domain),
+			'desc' => __('Formula parsable by Formula Parser above. Docs: https://hoa-project.net/En/Literature/Hack/Math.html', $domain),
+			'id' => $prefix . 'formula',
+			'type' => 'text',
 			'column' => true,
-		) );
+		));
 
 		$cmb->add_field(array(
 			'name' => 'Formula Parser',
-			'id'   => $prefix . 'formula_parser',
+			'id' => $prefix . 'formula_parser',
 			'type' => 'select',
-			'options' => $options
-		) );
+			'options' => $options,
+		));
 
-		$cmb->add_field( array(
-			'name'       => esc_html__( 'Result Prefix', $domain ),
-			'desc'       => esc_html__( 'Text to put before result', $domain ),
-			'id'         => $prefix . 'result_prefix',
-			'type'       => 'text',
+		$cmb->add_field(array(
+			'name' => __('Result Prefix', $domain),
+			'desc' => __('Text to put before result', $domain),
+			'id' => $prefix . 'result_prefix',
+			'type' => 'text',
 			'sanitization_cb' => array('ShortCalc\Calculators\WPPostCalculator', 'sanitizeText'),
 			'column' => true,
-		) );
+		));
 
-		$cmb->add_field( array(
-			'name'       => esc_html__( 'Result Postfix', $domain ),
-			'desc'       => esc_html__( 'Text to put after result', $domain ),
-			'id'         => $prefix . 'result_postfix',
-			'type'       => 'text',
+		$cmb->add_field(array(
+			'name' => __('Result Postfix', $domain),
+			'desc' => __('Text to put after result', $domain),
+			'id' => $prefix . 'result_postfix',
+			'type' => 'text',
 			'sanitization_cb' => array('ShortCalc\Calculators\WPPostCalculator', 'sanitizeText'),
 			'column' => true,
-		) );
+		));
 
-		$group_parameters = $cmb->add_field( array(
-			'id'          => $prefix . 'parameters',
-			'type'        => 'group',
+		$group_parameters = $cmb->add_field(array(
+			'id' => $prefix . 'parameters',
+			'type' => 'group',
 			'description' => __('Parameter meta data', $domain),
-			// 'repeatable'  => false, // use false if you want non-repeatable group
-			'options'     => array(
-				'group_title'   => __( 'Parameter {#}', $domain ), // since version 1.1.4, {#} gets replaced by row number
-				'add_button'    => __( 'Add Another Parameter', $domain ),
-				'remove_button' => __( 'Remove Parameter', $domain ),
-				'sortable'      => true, // beta
-				// 'closed'     => true, // true to have the groups closed by default
+			'options' => array(
+				'group_title' => __('Parameter {#}', $domain),
+				'add_button' => __('Add Another Parameter', $domain),
+				'remove_button' => __('Remove Parameter', $domain)
 			),
-		) );
+		));
 
 		$cmb->add_group_field( $group_parameters, array(
 			'name' => 'Name',
 			'description' => __('Must match a parameter in the formula', $domain),
-			'id'   => 'name',
+			'id' => 'name',
 			'type' => 'text',
-		) );
+		));
 
 		$cmb->add_group_field( $group_parameters, array(
 			'name' => 'Type of Element',
-			'id'   => 'element',
+			'id' => 'element',
 			'type' => 'select',
 			'default' => 'input',
 			'options' => array(
 				'input' => __('Input', $domain),
 				'select' => __('Select', $domain),
 			),
-		) );
+		));
 
 		$cmb->add_group_field( $group_parameters, array(
 			'name' => 'Label',
-			'id'   => 'label',
+			'id' => 'label',
 			'description' => __('The label is used on the calculator frontend', $domain),
 			'type' => 'text',
-		) );
+		));
 
 		$cmb->add_group_field( $group_parameters, array(
 			'name' => 'Prefix',
-			'id'   => 'prefix',
+			'id' => 'prefix',
 			'description' => __('Text to put before field on form', $domain),
 			'sanitization_cb' => array('ShortCalc\Calculators\WPPostCalculator', 'sanitizeText'),
-			'type' => 'text',
-		) );
+			'type' => 'text'
+		));
 
 		$cmb->add_group_field( $group_parameters, array(
 			'name' => 'Postfix',
-			'id'   => 'postfix',
+			'id' => 'postfix',
 			'description' => __('Text to put after field on form', $domain),
 			'sanitization_cb' => array('ShortCalc\Calculators\WPPostCalculator', 'sanitizeText'),
 			'type' => 'text',
-		) );
+		));
 
 		$cmb->add_group_field( $group_parameters, array(
 			'name' => 'Attributes',
 			'description' => __('HTML attributes, for example: value="Foo" required type="submit"', $domain),
-			'id'   => 'attributes',
+			'id' => 'attributes',
 			'type' => 'textarea_small',
-		) );
+		));
 
 		$cmb->add_group_field( $group_parameters, array(
 			'name' => 'Options',
 			'description' => __('Selectable options for select elements', $domain),
-			'id'   => 'options',
+			'id' => 'options',
 			'type' => 'text',
 			'repeatable' => true,
-		) );
+		));
 	}
 
 	public static function find(String $name) {
